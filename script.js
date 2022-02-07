ed1fa7bd464636830c62f2aefa400d0e322c6629
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-02-07 19:07:01 +0500

    Create empty functions
    Create empty functions that will be used later

---
+++ script.js
@@ -0,0 +1,22 @@
+// rock paper scissors (or RPS)
+
+function game(numOfRounds) {
+// play 5 non-tie rounds
+// keep score
+// report winner and loser
+  function playRound(playerSelection, computerSelection) {
+    //play one round of RPS and return win/lose/tie - string
+  }
+}
+
+//return randomly either Rock, Paper or Scissors
+function computerPlay() {
+    // 0-rock, 1-paper, 2-scissors
+
+}
+
+function playerPlay() {
+  // query to input Rock, Scissors or Paper
+  // uppercase input
+  // return input 
+}
