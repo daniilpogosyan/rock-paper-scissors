fb1fa17b4884fe12b671e58e56c6759499803e46
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-02-07 19:30:46 +0500

    Complete logic of playRound()

--- script.js
+++ script.js
@@ -4,8 +4,30 @@ function game(numOfRounds) {
 // play 5 non-tie rounds
 // keep score
 // report winner and loser
+  let playerSelection = playerPlay();
+  let computerSelection = computerPlay();
+  let outcome = playRound(playerSelection, computerSelection);
+  console.log(outcome);
+
   function playRound(playerSelection, computerSelection) {
     //play one round of RPS and return win/lose/tie - string
+    switch (true) {
+      //win case
+      case playerSelection == 'ROCK'      && computerSelection == 'SCISSORS':
+      case playerSelection == 'PAPER'     && computerSelection == 'ROCK':
+      case playerSelection == 'SCISSORS'  && computerSelection == 'PAPER':
+        return `You win: ${playerSelection} beat ${computerSelection}`;
+  
+      //lose case
+      case playerSelection == 'SCISSORS'  && computerSelection == 'ROCK':
+      case playerSelection == 'ROCK'      && computerSelection == 'PAPER':
+      case playerSelection == 'PAPER'     && computerSelection == 'SCISSORS':
+        return `You lose: ${computerSelection} beat ${playerSelection}`;
+        
+      //tie
+      case playerSelection == computerSelection:
+        return `It's a tie: both picked ${playerSelection}`;
+    }    
   }
 }
 
